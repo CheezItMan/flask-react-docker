176f1dc24705c0db5a32a62cd60c4eafcaea99b0
Chris McAnally <chris@adadevelopersacademy.org>
2021-06-20 21:59:10 -0700

    add encode token test

--- services/users/src/tests/test_user_model.py
+++ services/users/src/tests/test_user_model.py
@@ -1,8 +1,13 @@
 def test_passwords_are_random(test_app, test_database, add_user):
     """
-        Make two users with the same password and verify
-        that the encrypted passwords are different.
+    Make two users with the same password and verify
+    that the encrypted passwords are different.
     """
-    user_one = add_user('justatest', 'test@test.com', 'greaterthaneight')
-    user_two = add_user('justatest2', 'test@test2.com', 'greaterthaneight')
+    user_one = add_user("justatest", "test@test.com", "greaterthaneight")
+    user_two = add_user("justatest2", "test@test2.com", "greaterthaneight")
     assert user_one.password != user_two.password
+
+def test_encode_token(test_app, test_database, add_user):
+    user = add_user('justatest', 'test@test.com', 'test')
+    token = user.encode_token(user.id)
+    assert isinstance(token, str)
